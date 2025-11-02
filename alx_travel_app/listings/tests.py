892426a9aaf31d3998e6ec8b29c685ce2cd10aa3
Sama Mostafa <231562182+SamaMostafa-Programmer@users.noreply.github.com>
2025-11-02 15:18:59 +0200

    Delete alx_travel_app/listings directory

--- alx_travel_app/listings/tests.py
+++
@@ -1,6 +0,0 @@
-from django.test import TestCase
-
-class SimpleTest(TestCase):
-    def test_home_view(self):
-        response = self.client.get('/')
-        self.assertEqual(response.status_code, 404)  # no root URL defined yet
