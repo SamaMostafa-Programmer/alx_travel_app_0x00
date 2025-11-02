deb8fc56c1ee8570e9c4785d4f8910b94247cf60
Sama Mostafa <231562182+SamaMostafa-Programmer@users.noreply.github.com>
2025-11-02 07:17:34 +0200

    Update models.py

--- alx_travel_app/listings/models.py
+++ alx_travel_app/listings/models.py
@@ -1,1 +1,9 @@
+from django.db import models
 
+# Placeholder for future models
+class Listing(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField(blank=True)
+
+    def __str__(self):
+        return self.title
